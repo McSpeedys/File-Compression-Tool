cdcd050705a5b66c98d7a5812b08020109fe0b82
McSpeedys <eesenogl@asu.edu>
2025-10-24 13:12:05 -0700

    Implemented the initial version of main and compressor.h

--- compressor.h
+++ compressor.h
@@ -1,7 +1,14 @@
+//Protection.
 #ifndef COMPRESSOR_H
 #define COMPRESSOR_H
 
+//Necessary Libraries.
 #include <iostream>
 #include <fstream>
+
+//Prototype function declarations.
+void compress(ifstream& infile, ofstream& outfile)
+void decompress(ifstream& infile, ofstream& outfile)
+int charCount(char tgtchar, char line[256])
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,8 +1,50 @@
 #include "compressor.h"
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 
 int main(int argc, char** argv){
+  //Using a switch to handle user input.
+  switch argv[1]{
+    case "-c":{
+      std::ifstream infile;
+      std::ofstream outfile;
 
+      infile.open(argv[2]);
+      outfile.open(argv[3]);
+
+      //Checking if the files given exist.
+      if(!(infile.is_open() && outfile.is_open())){
+        std::cout << "Please input valid and existing files." << std::endl;
+        return 1;
+      }
+
+      compress(infile, outfile);
+
+      break;
+    }
+    case "-d":{
+      std::ifstream infile;
+      std::ofstream outfile;
+
+      infile.open(argv[2]);
+      outfile.open(argv[3]);
+
+      if(!(infile.is_open() && outfile.is_open())){
+        std::cout << "Please input valid and existing files." << std::endl;
+      }
+
+      decompress(infile, outfile);
+
+      break;
+    }
+    default:{
+      std::cout << "Valid Usage:\n";
+      std::cout << "To Compress: ./compress.exe -c <Your_input_file> <Your_output_file>\n";
+      std::cout << "To Decompress: ./compress.exe -d <Your_input_file> <Your_output_file>" << std::endl;
+      return 1;
+    }
+
+  }
   return 0;
 }
