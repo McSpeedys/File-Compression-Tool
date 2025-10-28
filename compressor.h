0c4b53285bde10239c8aa8d4bdf385f1e8eeae24
McSpeedys <eesenogl@asu.edu>
2025-10-28 13:05:54 -0700

    Updated .h file.

--- compressor.h
+++ compressor.h
@@ -5,10 +5,12 @@
 //Necessary Libraries.
 #include <iostream>
 #include <fstream>
+#include <unordered_map>
 
 //Prototype function declarations.
 void compress(ifstream& infile, ofstream& outfile)
 void decompress(ifstream& infile, ofstream& outfile)
-int charCount(char tgtchar, char line[256])
+int charCount(std::unordered_map<char, int>& charMap)
+void generateCode()
 
 #endif
